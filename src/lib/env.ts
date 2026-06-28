370a9e45b04bab795e5c18af748c134098e29942
Syariffullah <dinnereatc@gmail.com>
2026-06-28 08:45:55 +0700

    init(project): initialize project with Tanstack Start

---
+++ src/lib/env.ts
@@ -0,0 +1,14 @@
+function getEnv(name: keyof ImportMetaEnv): string {
+  const value = import.meta.env[name]
+
+  if (!value) {
+    throw new Error(`Missing environment variable: ${name}`)
+  }
+
+  return value
+}
+
+export const env = {
+  SUPABASE_URL: getEnv('VITE_SUPABASE_URL'),
+  SUPABASE_ANON_KEY: getEnv('VITE_SUPABASE_ANON_KEY'),
+}
